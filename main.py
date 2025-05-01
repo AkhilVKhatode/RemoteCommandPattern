c35944a11d4e1d7c61d1b6c7f2f223a5db8bccf8
Akhil Vishwanath Khatode <35483653+AkhilVKhatode@users.noreply.github.com>
2025-05-01 14:27:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -56,12 +56,20 @@ class RemoteControl:
     def __init__(self):
         self.on_command = None
         self.off_command = None
+        self.channel_command = None
+        self.volume_command = None
 
     def set_on_command(self, on_command):
         self.on_command = on_command
 
     def set_off_command(self, off_command):
         self.off_command = off_command
+
+    def set_channel_command(self, channel_command):
+        self.channel_command = channel_command
+
+    def set_volume_command(self, volume_command):
+        self.volume_command = volume_command
 
     def press_on_button(self):
         if self.on_command:
@@ -71,24 +79,32 @@ class RemoteControl:
         if self.off_command:
             self.off_command.execute()
 
+    def press_channel_button(self):
+        if self.channel_command:
+            self.channel_command.execute()
+
+    def press_volume_button(self):
+        if self.volume_command:
+            self.volume_command.execute()
+
 # Main code
 if __name__ == "__main__":
     tv = TV()
-    
+
     # Create commands
     turn_on = TurnOnCommand(tv)
     turn_off = TurnOffCommand(tv)
     change_channel = ChangeChannelCommand(tv, 5)
     adjust_volume = AdjustVolumeCommand(tv, 20)
-    
+
     # Create remote control
     remote = RemoteControl()
     remote.set_on_command(turn_on)
     remote.set_off_command(turn_off)
-    
+    remote.set_channel_command(change_channel)
+    remote.set_volume_command(adjust_volume)
+
     remote.press_on_button()  # Turn on the TV
     remote.press_off_button()  # Turn off the TV
-    
-    # Execute other commands
-    change_channel.execute()  # Change the channel
-    adjust_volume.execute()  # Adjust the volume
+    remote.press_channel_button()  # Change the channel
+    remote.press_volume_button()  # Adjust the volume
